d56e6e5b3a891a8bb67f04c1d4744b6ddbc6afee
Emil Kronborg Andersen <emil.kronborg@protonmail.com>
2022-05-07 23:18:30 +0200

    Project 04: add solutions

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,27 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+@R2
+M = 0 // Clear the output
+
+(LOOP)
+    @R1
+    D = M     // Load R1 (our multiplier) into the D register
+    @END
+    D;JEQ     // Jump to END if R1 = 0
+
+    @R0
+    D = M     // Load R0 (our multiplicand) into the D register
+
+    @R2       // Point the A register at the result
+    M = D + M // Add the D value to our result
+
+    @R1
+    M = M - 1 // Subtract 1 from our multiplier
+
+    @LOOP
+    0;JMP     // Jump to LOOP. Note that we exit the loop when R1 = 0 above!
+
+(END)
+	@END
+	0;JMP
